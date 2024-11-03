04cc93c3bb54dc932c5e9f09f915341a84e2f974
Dmitry Yatskevich <dima.ja06@gmail.com>
2024-11-03 19:41:37 +0300

    Create Layout

---
+++ src/layouts/Layout.tsx
@@ -0,0 +1,15 @@
+import { Footer, Header } from '../components';
+import { Outlet } from 'react-router-dom';
+import { Box, Container } from '@mui/material';
+
+export function Layout() {
+	return (
+		<Box sx={{ display: 'flex', flexDirection: 'column', height: '100vh' }}>
+			<Header />
+			<Container component="main" sx={{ flexGrow: 1, p: 4 }}>
+				<Outlet />
+			</Container>
+			<Footer />
+		</Box>
+	);
+}
