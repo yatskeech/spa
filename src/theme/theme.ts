4dd7f0d8432de3ebf2e9970f142d1c73f5eddcde
Dmitry Yatskevich <dima.ja06@gmail.com>
2024-11-04 11:46:24 +0300

    Create custom theme

---
+++ src/theme/theme.ts
@@ -0,0 +1,13 @@
+import { createTheme } from '@mui/material';
+
+export const theme = createTheme({
+	palette: {
+		mode: 'light',
+		primary: {
+			main: '#435ea4',
+		},
+		secondary: {
+			main: '#49526c',
+		},
+	},
+});
