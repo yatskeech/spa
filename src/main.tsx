66c7a20af7460bc78ea92eeaf661a1d36f115cae
Dmitry Yatskevich <dima.ja06@gmail.com>
2024-11-06 20:02:09 +0300

    Add theme and route

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.scss';
+import { RouterProvider } from 'react-router-dom';
+import { ThemeProvider } from '@mui/material';
+import { router } from './router';
+import { theme } from './theme';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+	<StrictMode>
+		<ThemeProvider theme={theme}>
+			<RouterProvider router={router} />
+		</ThemeProvider>
+	</StrictMode>
+);
