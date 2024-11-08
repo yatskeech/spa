6755bb387258de3f84f7a1787d17d8907ddc7374
Dmitry Yatskevich <dima.ja06@gmail.com>
2024-11-08 21:12:06 +0300

    Delete Await and Suspense from AlbumListPage

--- src/pages/AlbumListPage.tsx
+++ src/pages/AlbumListPage.tsx
@@ -1,41 +1,29 @@
 import { Container, Grid2 as Grid, Link } from '@mui/material';
 import { Title } from '../components';
-import { useLoaderData, Await, Link as RouterLink } from 'react-router-dom';
-import { Album } from '../interfaces';
-import { Suspense } from 'react';
-import { LoadingPage } from './LoadingPage.tsx';
+import { useLoaderData, Link as RouterLink } from 'react-router-dom';
+import { Album } from '../types';
 import CollectionsIcon from '@mui/icons-material/Collections';
 
-export function AlbumsPage() {
-	const { albums } = useLoaderData() as { albums: Promise<Album[]> };
+export function AlbumListPage() {
+	const albums = useLoaderData() as Album[];
 
 	return (
 		<Container>
-			<Suspense fallback={<LoadingPage />}>
-				<Title>Albums</Title>
-				<Await resolve={albums}>
-					{(resolvedAlbums: Album[]) => (
-						<Grid container columnSpacing={4} rowSpacing={1} direction="row">
-							{resolvedAlbums.map(({ id, title }) => (
-								<Grid
-									key={id}
-									size={6}
-									sx={{ display: 'flex', alignItems: 'center', gap: 1 }}
-								>
-									<CollectionsIcon color="secondary" />
-									<Link
-										component={RouterLink}
-										to={id.toString()}
-										color="secondary"
-									>
-										{title}
-									</Link>
-								</Grid>
-							))}
-						</Grid>
-					)}
-				</Await>
-			</Suspense>
+			<Title>Albums</Title>
+			<Grid container columnSpacing={4} rowSpacing={1} direction="row">
+				{albums.map(({ id, title }) => (
+					<Grid
+						key={id}
+						size={6}
+						sx={{ display: 'flex', alignItems: 'center', gap: 1 }}
+					>
+						<CollectionsIcon color="secondary" />
+						<Link component={RouterLink} to={id.toString()} color="secondary">
+							{title}
+						</Link>
+					</Grid>
+				))}
+			</Grid>
 		</Container>
 	);
 }
